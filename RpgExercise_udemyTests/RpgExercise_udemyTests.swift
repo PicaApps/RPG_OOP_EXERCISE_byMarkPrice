7639d36ff7fadb97379f9fac210de0d3515a9809
Marius Butnariuc-Fiter <marius.butnariuc@gmail.com>
2016-01-15 06:51:35 +0200

    Initial Commit

---
+++ RpgExercise_udemyTests/RpgExercise_udemyTests.swift
@@ -0,0 +1,36 @@
+//
+//  RpgExercise_udemyTests.swift
+//  RpgExercise_udemyTests
+//
+//  Created by Marius Butnariuc-Fiter on 15/01/16.
+//  Copyright © 2016 Marius Butnariuc-Fiter. All rights reserved.
+//
+
+import XCTest
+@testable import RpgExercise_udemy
+
+class RpgExercise_udemyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
